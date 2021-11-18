043222b671e835961486c2a1571afa59d8326a0d
Jan Kalivoda <jan.kalivoda@ackee.cz>
2021-11-18 15:09:54 +0100

    ✨ Add better test

--- tests/test_fuzzy.py
+++ tests/test_fuzzy.py
@@ -1,3 +1,5 @@
+import math
+from math import ceil
 from brownie import accounts
 from brownie.test import given, strategy
 
@@ -12,3 +14,14 @@ def test_log_functions(Math,MathTrusted, value):
     res2 = c2.log2Trusted(value)
 
     assert res1.return_value == res2.return_value
+
+@given(
+    value=strategy('uint256', min_value=1),
+)
+def test_log_functions_better(Math, value):
+    c1 = Math.deploy({'from': accounts[0]})
+
+    res1 = c1.log2Untrusted(value)
+    res2 = ceil(math.log(value,2))
+
+    assert res1.return_value == res2
