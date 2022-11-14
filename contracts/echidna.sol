4637097315a0a510ffe4b62cb352ed27b6e5a643
Michal Převrátil <michprev@gmail.com>
2022-11-14 22:14:14 +0100

    ✨ Add echidna solution

--- contracts/echidna.sol
+++ contracts/echidna.sol
@@ -3,5 +3,7 @@ pragma solidity >=0.4.20 <0.8.0;
 import "./overflow.sol";
 
 contract TEST is Overflow {
-
+    function checkBalance() public {
+        assert(actualBalance >= lastBalance);
+    }
 }
